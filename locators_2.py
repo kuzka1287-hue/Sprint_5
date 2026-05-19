4a1213f458f2723d956657b240fb85fc7caf0dc3
kuzka1287-hue <kuzka1287@gmail.com>
2026-05-19 09:07:27 +0300

    Create locators_2.py

---
+++ locators_2.py
@@ -0,0 +1,37 @@
+from selenium.webdriver.common.by import By
+
+class MainPageLocators:
+    LOGIN_BUTTON_MAIN = (By.XPATH, "//button[text()='Войти в аккаунт']")
+    PERSONAL_ACCOUNT_BUTTON = (By.XPATH, "//p[text()='Личный кабинет']")
+    CONSTRUCTOR_BUTTON = (By.XPATH, "//p[text()='Конструктор']")
+    LOGO = (By.XPATH, "//div[@class='AppHeader_header__logo__2D0X2']")
+    BUNS_TAB = (By.XPATH, "//span[text()='Булки']")
+    SAUCES_TAB = (By.XPATH, "//span[text()='Соусы']")
+    FILLINGS_TAB = (By.XPATH, "//span[text()='Начинки']")
+    ACTIVE_TAB = (By.XPATH, "//div[contains(@class, 'tab_tab_type_current__2BEPc')]")
+
+class RegisterPageLocators:
+    NAME_INPUT = (By.XPATH, "//label[text()='Имя']/following-sibling::input")
+    EMAIL_INPUT = (By.XPATH, "//label[text()='Email']/following-sibling::input")
+    PASSWORD_INPUT = (By.XPATH, "//label[text()='Пароль']/following-sibling::input")
+    REGISTER_BUTTON = (By.XPATH, "//button[text()='Зарегистрироваться']")
+    PASSWORD_ERROR = (By.XPATH, "//p[text()='Некорректный пароль']")
+
+class LoginPageLocators:
+    EMAIL_INPUT = (By.XPATH, "//input[@name='name']")
+    PASSWORD_INPUT = (By.XPATH, "//input[@name='Пароль']")
+    LOGIN_BUTTON = (By.XPATH, "//button[text()='Войти']")
+    REGISTER_LINK = (By.XPATH, "//a[text()='Зарегистрироваться']")
+    FORGOT_PASSWORD_LINK = (By.XPATH, "//a[text()='Восстановить пароль']")
+
+class ForgotPasswordPageLocators:
+    LOGIN_BUTTON = (By.XPATH, "//a[text()='Войти']")
+
+class PersonalAccountLocators:
+    LOGOUT_BUTTON = (By.XPATH, "//button[text()='Выйти']")
+    ORDER_HISTORY = (By.XPATH, "//a[text()='История заказов']")
+
+class ConstructorLocators:
+    BUN_SECTION = (By.XPATH, "//h2[text()='Булки']/..")
+    SAUCE_SECTION = (By.XPATH, "//h2[text()='Соусы']/..")
+    FILLING_SECTION = (By.XPATH, "//h2[text()='Начинки']/..")
