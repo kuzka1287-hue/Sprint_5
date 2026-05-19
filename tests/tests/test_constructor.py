616a6287901fc2971b7ecbcdf7777d4ec5669784
kuzka1287-hue <kuzka1287@gmail.com>
2026-05-19 09:36:26 +0300

    Create test_constructor.py

---
+++ tests/tests/test_constructor.py
@@ -0,0 +1,26 @@
+import pytest
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from locators import MainPageLocators, ConstructorLocators
+from data import BASE_URL
+
+class TestConstructor:
+    def test_switch_to_buns_tab(self, driver):
+        driver.get(BASE_URL)
+        driver.find_element(*MainPageLocators.SAUCES_TAB).click()  # переключаем на Соусы
+        driver.find_element(*MainPageLocators.BUNS_TAB).click()
+        # Активный таб – Булки
+        active_tab = driver.find_element(*MainPageLocators.ACTIVE_TAB).text
+        assert active_tab == "Булки"
+
+    def test_switch_to_sauces_tab(self, driver):
+        driver.get(BASE_URL)
+        driver.find_element(*MainPageLocators.SAUCES_TAB).click()
+        active_tab = driver.find_element(*MainPageLocators.ACTIVE_TAB).text
+        assert active_tab == "Соусы"
+
+    def test_switch_to_fillings_tab(self, driver):
+        driver.get(BASE_URL)
+        driver.find_element(*MainPageLocators.FILLINGS_TAB).click()
+        active_tab = driver.find_element(*MainPageLocators.ACTIVE_TAB).text
+        assert active_tab == "Начинки"
