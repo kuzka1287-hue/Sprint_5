76ea8518d82f1087808b98c5417fcb34841983a5
kuzka1287-hue <kuzka1287@gmail.com>
2026-05-25 20:08:53 +0300

    Update locators.py

--- Sprint_5/TESTS_1/locators.py
+++ Sprint_5/TESTS_1/locators.py
@@ -4,14 +4,13 @@ class MainPageLocators:
     LOGIN_BUTTON_MAIN = (By.XPATH, "//button[text()='Войти в аккаунт']")
     PERSONAL_ACCOUNT_BUTTON = (By.XPATH, "//p[text()='Личный кабинет']")
     CONSTRUCTOR_BUTTON = (By.XPATH, "//p[text()='Конструктор']")
-    LOGO = (By.CSS_SELECTOR, ".AppHeader_header__logo__2D0X2")          # более надёжный
+    LOGO = (By.CSS_SELECTOR, ".AppHeader_header__logo__2D0X2")
     BUNS_TAB = (By.XPATH, "//span[text()='Булки']")
     SAUCES_TAB = (By.XPATH, "//span[text()='Соусы']")
     FILLINGS_TAB = (By.XPATH, "//span[text()='Начинки']")
-    ACTIVE_TAB = (By.XPATH, "//div[contains(@class, 'tab_tab_type_current')]")  # убран динамический суффикс
+    ACTIVE_TAB = (By.XPATH, "//div[contains(@class, 'tab_tab_type_current')]")
 
 class RegisterPageLocators:
-    # Используем атрибут name, где возможно, или стабильные CSS
     NAME_INPUT = (By.NAME, "name")
     EMAIL_INPUT = (By.NAME, "email")
     PASSWORD_INPUT = (By.NAME, "password")
@@ -24,10 +23,11 @@ class LoginPageLocators:
     LOGIN_BUTTON = (By.XPATH, "//button[text()='Войти']")
     REGISTER_LINK = (By.XPATH, "//a[text()='Зарегистрироваться']")
     FORGOT_PASSWORD_LINK = (By.XPATH, "//a[text()='Восстановить пароль']")
+    LOGIN_LINK_ON_REGISTER_PAGE = (By.XPATH, "//a[text()='Войти']")
 
 class ForgotPasswordPageLocators:
     LOGIN_BUTTON = (By.XPATH, "//a[text()='Войти']")
 
 class PersonalAccountLocators:
     LOGOUT_BUTTON = (By.XPATH, "//button[text()='Выйти']")
-    ORDER_HISTORY = (By.XPATH, "//a[text()='История заказов']") 
+    ORDER_HISTORY = (By.XPATH, "//a[text()='История заказов']")
