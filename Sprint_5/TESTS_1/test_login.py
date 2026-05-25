f4ca361c99ddc112989cbe250024d9a274f21fc7
kuzka1287-hue <kuzka1287@gmail.com>
2026-05-25 20:19:23 +0300

    Update test_login.py

--- Sprint_5/TESTS_1/test_login.py
+++ Sprint_5/TESTS_1/test_login.py
@@ -38,4 +38,4 @@ class TestLogin:
         driver.find_element(*LoginPageLocators.EMAIL_INPUT).send_keys(registered_user["email"])
         driver.find_element(*LoginPageLocators.PASSWORD_INPUT).send_keys(registered_user["password"])
         driver.find_element(*LoginPageLocators.LOGIN_BUTTON).click()
-        assert driver.find_element(*MainPageLocators.CONSTRUCTOR_BUTTON).is_displayed()
+        assert driver.find_element(*MainPageLocators.CONSTRUCTOR_BUTTON).is_displayed() 
