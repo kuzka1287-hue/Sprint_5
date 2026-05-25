1e86a55b75b2966eaefdc547392d5f06c2b4d013
kuzka1287-hue <kuzka1287@gmail.com>
2026-05-25 20:15:13 +0300

    Update test_constructor.py

--- Sprint_5/TESTS_1/test_constructor.py
+++ Sprint_5/TESTS_1/test_constructor.py
@@ -1,15 +1,13 @@
-import pytest
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-from locators import MainPageLocators, ConstructorLocators
+from locators import MainPageLocators
 from data import BASE_URL
 
 class TestConstructor:
     def test_switch_to_buns_tab(self, driver):
         driver.get(BASE_URL)
-        driver.find_element(*MainPageLocators.SAUCES_TAB).click()  # переключаем на Соусы
+        driver.find_element(*MainPageLocators.SAUCES_TAB).click()
         driver.find_element(*MainPageLocators.BUNS_TAB).click()
-        # Активный таб – Булки
         active_tab = driver.find_element(*MainPageLocators.ACTIVE_TAB).text
         assert active_tab == "Булки"
 
@@ -24,4 +22,3 @@ class TestConstructor:
         driver.find_element(*MainPageLocators.FILLINGS_TAB).click()
         active_tab = driver.find_element(*MainPageLocators.ACTIVE_TAB).text
         assert active_tab == "Начинки"
-  
